662f55d2fbe2622f5ac802a90526d12f4eaf7316
ARS-saad <abdurrahmansaad46@gmail.com>
2023-12-16 22:10:40 +0600

    There have do most of this project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/main/Navbar";
+import StarCanvas from "@/components/main/StarCanvas";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,6 +21,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <StarCanvas />
+        <Navbar />
         {children}
       </body>
     </html>
